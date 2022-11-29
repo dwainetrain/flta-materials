7526b7efb7af43e787b6393e4fb62255a02e4f4b
Dwaine Best <dwaine.best@gmail.com>
2022-11-29 08:24:42 -0800

    Start of chapter 12

---
+++ 11-networking-in-flutter/projects/starter/lib/network/recipe_service.dart
@@ -0,0 +1,24 @@
+import 'dart:developer';
+
+import 'package:http/http.dart';
+
+const String apiKey = 'ace60248ff848b7f8aa6532ea0258f48	';
+const String apiId = 'ddb93db9';
+const String apiUrl = 'https://api.edamam.com/search';
+
+class RecipeService {
+  Future getData(String url) async {
+    final response = await get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      log(response.body);
+    }
+  }
+
+  Future<dynamic> getRecipes(String query, int from, int to) async {
+    final recipeData = await getData(
+        '$apiUrl?app_id=$apiId&app_key=$apiKey&q=$query&from=$from&to=$to');
+    return recipeData;
+  }
+}
